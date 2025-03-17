779e3b6bf23e0a20bdda2d488ead4cd422b3601c
Dimitris Sparagis <dimsparagis@gmail.com>
2025-03-17 17:29:19 +0200

    Exported data in separate file

---
+++ data/data.ts
@@ -0,0 +1,69 @@
+export const hashtags = [
+    {
+        name: "mountains",
+        numOfPosts: 10
+    },
+    {
+        name: "nature",
+        numOfPosts: 5
+    },
+    {
+        name: "travel",
+        numOfPosts: 3
+    },
+    {
+        name: "adventure",
+        numOfPosts: 2
+    }
+]
+
+export const stats = [
+    {
+        name: "Likes",
+        stat: 100
+    },
+    {
+        name: "Comments",
+        stat: 50
+    },
+    {
+        name: "Posts",
+        stat: 10
+    }   
+];
+
+export const recommendedAccounts = [
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    }, 
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    {
+        name: "Dimitris Sparagis",
+        username: "@dimapragis"
+    },
+    
+]

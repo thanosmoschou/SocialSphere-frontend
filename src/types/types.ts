bb48e7e9107af5c821c119d703175c6d7bdcad4e
Dimitris Sparagis <dimsparagis@gmail.com>
2025-05-01 17:00:02 +0300

    Feature: Extracted all types in a single file

---
+++ src/types/types.ts
@@ -0,0 +1,53 @@
+export type User = {
+   userId:number;
+   email:string;
+   createdAt:string;
+   bio:string;
+   location:string;
+   profileName:string;
+   displayName:string;
+   posts:Post[];
+   role:string;
+   followers:[];
+   following:[];
+   skills: string[];
+   interests: string[];
+   userLinks: UserLink[];
+}
+
+export type UserLink = {
+   name: string;
+   url: string;
+}
+
+export type Post = {
+   title: string;
+   description: string | null;
+   imageUrl: string | null;
+   creatorId: number | null;
+   date: string;
+   postId: number;
+   usersLiked: number[];
+   hashtags: string[];
+   comments: Comment[];
+};
+
+export type Comment = {
+   commentId: number;
+   content: string;
+   userCommented: number;
+   date: string;
+};
+
+export type Hashtag = {
+   hashtagId: number;
+   name: string;
+};
+
+export type CommentDTO = {
+   content: string;
+   postId: number;
+   userId: number;
+};
+
+

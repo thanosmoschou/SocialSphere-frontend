8067bc800be9524823e81b253ba140cea709f1eb
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2025-06-02 17:08:27 +0300

    edit backend url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const backendUrl = "https://socialsphere-backend-w43u.onrender.com";
+export const backendUrl = "https://socialsphere-backend.fly.dev";
 // export const backendUrl = "http://localhost:8080";
 export const apiUrl = `${backendUrl}/api/v1/auth`;

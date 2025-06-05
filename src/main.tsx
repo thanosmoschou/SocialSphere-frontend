4b2b28122021a5513a966dd96d24ad64e818f23d
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2025-06-05 18:28:14 +0300

    env

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,11 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
-import { APP_BASE_URL } from './lib/constants.ts';
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
       <QueryClientProvider client={queryClient}>
-         <BrowserRouter basename={APP_BASE_URL}>
+         <BrowserRouter basename={import.meta.env.APP_BASE_URL}>
             <App />
          </BrowserRouter>
       </QueryClientProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { APP_BASE_URL } from './src/lib/constants';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  const BASE_URL_FOR_VITE = APP_BASE_URL;
+  const BASE_URL_FOR_VITE = process.env.VITE_APP_BASE_URL;
 
   return {
     plugins: [react(), tailwindcss()],
